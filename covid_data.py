34d0876e5fbd6c91e5434c967ae3ae3dbc49951c
lgh19 <67384606+lgh19@users.noreply.github.com>
2021-05-24 14:08:58 -0400

    Update covid_data.py

--- covid_data.py
+++ covid_data.py
@@ -11,6 +11,7 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 import re
 
+
 class CovidData:
     pasdaData = ""
     dohfile = ""
@@ -28,7 +29,6 @@ class CovidData:
         from bs4 import BeautifulSoup
         import requests
 
-
         r = requests.get("https://www.health.pa.gov/topics/disease/coronavirus/Pages/LTCF-Data.aspx")
         soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -44,71 +44,53 @@ class CovidData:
         assert len(doc_urls) == 4  # Verify that the web site only lists four XSLX files.
         self.doh_last_updated = doh_dates[0]
         fullurl = "https://www.health.pa.gov" + doc_urls[0]
-
+        print(fullurl)
         r = requests.get(fullurl)
 
-        open('DOH_Data.xlsx', 'w').write(r.content)
+        open('DOH_Data.xlsx', 'wb').write(r.content)
 
-        r = requests.get(
-            "https://www.health.pa.gov/topics/programs/immunizations/Pages/COVID-19-Vaccine-Providers.aspx")
-        soup = BeautifulSoup(r.text, 'html.parser')
-
-        doc_urls = []
-        fpp_dates = []
-        for link in soup.find_all('a'):
-            url = link.get('href', 'No Link Found')
-            if re.search('xlsx', url) is not None:
-                title = link.get('title')
-                fpp_dates.append(title)
-                doc_urls.append(url)
-
-        fullurl = "https://www.health.pa.gov" + doc_urls[5]
-
-        fpp_date = fpp_dates[5]
-        fpp_date = fpp_date.split('- ')
-        fpp_date = fpp_date[1]
-        self.fpp_last_updated = fpp_date
-        r = requests.get(fullurl)
-        open('FPP_Data.xlsx', 'w').write(r.content)
-
-        wb = load_workbook('FPP_Data.xlsx')
+        r = requests.get("https://data.pa.gov/api/views/iwiy-rwzp/rows.csv?accessType=DOWNLOAD&api_foundry=true")
+        open('FPP_Data.csv', 'wb').write(r.content)
+        
+    #    fullSite = requests.get("https://data.pa.gov/Health/COVID-19-Federal-Pharmacy-Partners-Long-Term-Care-/iwiy-rwzp")
+    #    soup = BeautifulSoup(fullSite.text, 'html.parser')
+     
+        
+      #  wb = load_workbook('FPP_Data.xlsx')
         wb2 = load_workbook('DOH_Data.xlsx')
-
-        ws = wb['contour-export']
+     #   print(wb.sheetnames)
+     #   ws = wb[wb.sheetnames[0]]
 
         ws2 = wb2['Sheet1']
 
-        df = pd.DataFrame(ws.values)
+     #   df = pd.DataFrame(ws.values)
 
-        df.rename(columns={0: u"Facility Name", 1: u'Address', 2: u"City", 3: u"County", 4: u"Zip Code", 5: u'Facility Type', 6: u"clinicdt1",
-                           7: u"clinicdt2", 8: u"clinicdt3", 9: u"clinicdt4", 10: u"clinicdt5", 11: u"clinicdt6",
-                           12: u"clinicdt7", 13: u"clinicdt8", 14: u"clinicdt9", 15: u"clinicdt10"}, inplace=True)
+     #   df.rename(
+      #      columns={0: u"Facility Name", 1: u'Address', 2: u"City", 3: u"County", 4: u"Zip Code", 5: u'Facility Type',
+     #                6: u"clinicdt1",
+     #                7: u"clinicdt2", 8: u"clinicdt3", 9: u"clinicdt4", 10: u"clinicdt5", 11: u"clinicdt6",
+      #               12: u"clinicdt7", 13: u"clinicdt8", 14: u"clinicdt9", 15: u"clinicdt10"}, inplace=True)
 
-        
         df2 = pd.DataFrame(ws2.values)
 
         df2.rename(columns={0: u"FACID", 1: u"NAME", 2: u"CITY", 3: u"COUNTY", 4: u"ALL_BEDS", 5: u"CURRENT_CENSUS",
                             6: u"Resident Cases to Display",
                             7: u"Resident Deaths to Display", 8: u"Staff Cases to Display"}, inplace=True)
 
-        df = df.iloc[1:]
+     #   df = df.iloc[1:]
         df2 = df2.iloc[1:]
-        
 
-        df.to_csv("FPP_CSV1.csv", encoding='utf-8')
+      #  df.to_csv("FPP_CSV1.csv", encoding='utf-8')
 
         df2.to_csv("DOH_CSV1.csv", encoding='utf-8')
-        self.fppdf = pd.read_csv('FPP_CSV1.csv')
+        self.fppdf = pd.read_csv('FPP_Data.csv')
         self.dohdf = pd.read_csv('DOH_CSV1.csv')
-
         self.addDOHData()
-        
 
     def addDOHData(self):
         print
-        print "Adding DOH Covid Data"
+        print("Adding DOH Covid Data")
         print
-
 
         def isNaN(string):
             return string != string
@@ -133,7 +115,7 @@ class CovidData:
                 if row.FACID != tempfacid:
                     pass
                 else:
-                    #print "match"
+                    # print "match"
                     tempbeds = row.ALL_BEDS
                     tempcensus = row.CURRENT_CENSUS
                     tempres = row.Resident_Cases_to_Display
@@ -141,92 +123,80 @@ class CovidData:
                     tempstaff = row.Staff_Cases_to_Display
 
                     self.ltcfdf.loc[self.ltcfdf['FACILITY_I'] == tempfacid, 'ALL_BEDS'] = tempbeds
-                  #  print(tempbeds)
                     self.ltcfdf.loc[self.ltcfdf['FACILITY_I'] == tempfacid, 'CURRENT_CENSUS'] = tempcensus
-                  #  print(tempcensus)
                     self.ltcfdf.loc[self.ltcfdf['FACILITY_I'] == tempfacid, 'Resident_Cases_to_Display'] = tempres
                     self.ltcfdf.loc[self.ltcfdf['FACILITY_I'] == tempfacid, 'Resident_Deaths_to_Display'] = tempresdeath
                     self.ltcfdf.loc[self.ltcfdf['FACILITY_I'] == tempfacid, 'Staff_Cases_to_Display'] = tempstaff
 
         self.ltcfdf['DOH_Data_Last_Updated'] = self.doh_last_updated
         self.ltcfdf = self.ltcfdf
-        print "Done Adding DOH Covid Data"
+        print("Done Adding DOH Covid Data")
         print
         self.addFPPData()
 
     def addFPPData(self):
-        self.ltcfdf['clinicdt1'] = ''
-        self.ltcfdf['clinicdt1'] = self.ltcfdf["clinicdt1"].astype(str)
-        self.ltcfdf['clinicdt2'] = ''
-        self.ltcfdf['clinicdt2'] = self.ltcfdf["clinicdt2"].astype(str)
-        self.ltcfdf['clinicdt3'] = ''
-        self.ltcfdf['clinicdt3'] = self.ltcfdf["clinicdt3"].astype(str)
-        self.ltcfdf['clinicdt4'] = ''
-        self.ltcfdf['clinicdt4'] = self.ltcfdf["clinicdt4"].astype(str)
-        self.ltcfdf['clinicdt5'] = ''
-        self.ltcfdf['clinicdt5'] = self.ltcfdf["clinicdt5"].astype(str)
-        self.ltcfdf['clinicdt6'] = ''
-        self.ltcfdf['clinicdt6'] = self.ltcfdf["clinicdt6"].astype(str)
-        self.ltcfdf['clinicdt7'] = ''
-        self.ltcfdf['clinicdt7'] = self.ltcfdf["clinicdt7"].astype(str)
-        self.ltcfdf['clinicdt8'] = ''
-        self.ltcfdf['clinicdt8'] = self.ltcfdf["clinicdt8"].astype(str)
-        self.ltcfdf['clinicdt9'] = ''
-        self.ltcfdf['clinicdt9'] = self.ltcfdf["clinicdt9"].astype(str)
-        self.ltcfdf['clinicdt10'] = ''
-        self.ltcfdf['clinicdt10'] = self.ltcfdf["clinicdt10"].astype(str)
-        print "Adding FPP Data"
+        print("Adding FPP Data")
         print
 
-        
         print
         for index, row in self.fppdf.iterrows():
-            tempStr = (self.fppdf.at[index, 'Facility Name'])
+            tempStr = (self.fppdf.at[index, 'Long Term Care Facility Name'])
             tempStr = str(tempStr)
-            tempfacility = self.fppdf.at[index, 'Facility Name']
-            tempaddress = self.fppdf.at[index, 'Address']
-            temptype = self.fppdf.at[index, 'Facility Type']
-            tempfirstc = self.fppdf.at[index, 'clinicdt1']
-            tempsecondc = self.fppdf.at[index, 'clinicdt2']
-            tempthirdc = self.fppdf.at[index, 'clinicdt3']
-            tempfourthc = self.fppdf.at[index, 'clinicdt4']
-            tempfifthc = self.fppdf.at[index, 'clinicdt5']
-            tempsixthc = self.fppdf.at[index, 'clinicdt6']
-            tempseventhc = self.fppdf.at[index, 'clinicdt7']
-            tempeightc = self.fppdf.at[index, 'clinicdt8']
-            temp9thc = self.fppdf.at[index, 'clinicdt9']
-            temp10thc = self.fppdf.at[index, 'clinicdt10']
+            tempfacility = self.fppdf.at[index, 'Long Term Care Facility Name']
+            tempaddress = self.fppdf.at[index, 'Street Address']
+            tempPharm = self.fppdf.at[index, 'Federal Pharmacy Partner']
+            date1 = self.fppdf.at[index, '1st Clinic Date']
+            date2 = self.fppdf.at[index, '2nd Clinic Date']
+            date3 = self.fppdf.at[index, '3rd Clinic Date']
+            date4 = self.fppdf.at[index, '4th Clinic Date']
+            date5 = self.fppdf.at[index, '5th Clinic Date']
+            date6 = self.fppdf.at[index, '6th Clinic Date']
+            date7 = self.fppdf.at[index, '7th Clinic Date']
+            date8 = self.fppdf.at[index, '8th Clinic Date']
+            date9 = self.fppdf.at[index, '9th Clinic Date']
+            date10 = self.fppdf.at[index, '10th Clinic Date']
+            tempTotal = self.fppdf.at[index, 'Total Doses Administered']
+            tempFirstDose = self.fppdf.at[index,'First Doses Administered']
+            tempSecondDose = self.fppdf.at[index,'Second Doses Adminstered']
+            tempTotalRes = self.fppdf.at[index, 'Total Resident Doses Administered']
+            tempTotalStaff = self.fppdf.at[index, 'Total Staff Doses Administered']
 
             for index, row in self.ltcfdf.iterrows():
                 Str1 = (self.ltcfdf.at[index, 'FACILITY_N'])
                 Str1 = str(Str1)
                 ratio = fuzz.ratio(Str1.lower(), tempStr.lower())
-                if (85 < ratio < 90):
-                    self.ltcfdf.at[index, 'flag_FPP'] = True
+                if (ratio >= 90):
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Facility Name FPP'] = tempfacility
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Federal Pharmacy Partner'] = tempPharm
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'First Clinic Date '] = date1
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Second Clinic Date'] = date2
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Third Clinic Date'] = date3
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Fourth Clinic Date'] = date4
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Fifth Clinic Date'] = date5
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Sixth Clinic Date'] = date6
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Seventh Clinic Date'] = date7
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Eighth Clinic Date'] = date8
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Ninth Clinic Date'] = date9
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Tenth Clinic Date'] = date10
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Total Doses Administered'] = tempTotal
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'First Doses Administered'] = tempFirstDose
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Second Doses Adminstered'] = tempSecondDose
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Total Resident Doses Administered'] = tempTotalRes
+                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Total Staff Doses Administered'] = tempTotalStaff
                     break
-                elif (ratio >= 90):
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'Facility'] = tempfacility
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt1'] = tempfirstc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt2'] = tempsecondc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt3'] = tempthirdc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt4'] = tempfourthc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt5'] = tempfifthc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicd6'] = tempsixthc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt7'] = tempseventhc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt8'] = tempeightc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicdt9'] = temp9thc
-                    self.ltcfdf.loc[self.ltcfdf['FACILITY_N'] == Str1, 'clinicd10'] = temp10thc
-                    break
-        
-        self.ltcfdf['FPP_Data_Last_Updated'] = self.fpp_last_updated
+
+      #  self.ltcfdf['FPP_Data_Last_Updated'] = self.fpp_last_updated
         self.ltcfdf = self.ltcfdf
-        print "Done Adding FPP Data"
+        print("Done Adding FPP Data")
         self.writeToFile()
 
     def writeToFile(self):
         print
         b = "COVID19_Vaccine_Data_LTCF.csv"
         b = str(b)
-        self.ltcfdf.to_csv(b, encoding = 'utf-8')
+        self.ltcfdf.to_csv(b, encoding='utf-8')
         print("output file: "),
         print(b)
+        return b
+
+a = CovidData("Pasda.csv")
